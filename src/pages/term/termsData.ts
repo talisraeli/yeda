bc7a5be2bd729a20fe0b63095273ec0d7a1cb0be
Jonathan <88664859+jonaprojects@users.noreply.github.com>
2023-08-26 15:11:39 +0300

    Remove empty keywords and unnecessary customMarkdownPath.
    Co-authored-by: Tal Israeli <talisraeli225@gmail.com>

--- src/pages/term/termsData.ts
+++ src/pages/term/termsData.ts
@@ -10,15 +10,11 @@ export const termsData: ITerm[] = [
     description: ` התחל להשתמש בתבנית ״ידע״. 
     למדו על הפרויקט ועל הטכנולוגיות והדרישות המקדימות שלו
     `,
-    customMarkdownPath: 'צעדים-ראשונים',
-    keywords: [],
   },
   {
     urlPath: 'התקנה',
     displayName: 'התקנה',
     description: 'התקן את הפרויקט לסביבה הלוקאלית שלך.',
-    customMarkdownPath: 'התקנה',
-    keywords: [],
   },
   {
     urlPath: 'מבנה-הפרויקט',
@@ -26,35 +22,25 @@ export const termsData: ITerm[] = [
     description: `למד את מבנה הפרויקט - תיקיות וקבצים חשובים שאותם נשנה לצורך התאמה אישית 
     של התבנית.
     `,
-    customMarkdownPath: 'מבנה-הפרויקט',
-    keywords: [],
   },
   {
     urlPath: 'הוסף-מונחים',
     displayName: 'הוסף מונחים',
     description: `תוכלו להוסיף מונחים חדשים לתפריט אתר בקלות רבה, באמצעות יצירה של דפים בפורמט markdown.`,
-    customMarkdownPath: 'הוסף-מונחים',
-    keywords: [],
   },
   {
     urlPath: 'עיצוב-אישי',
     displayName: 'עיצוב האתר',
     description: `התאמה אישית של עיצוב האתר באמצעות תכונות גלובליות תוך שימוש בפורמט sass.`,
-    customMarkdownPath: 'עיצוב-אישי',
-    keywords: [],
   },
   {
     urlPath: 'רכיבים-ויזואלים',
     displayName: 'רכיבים ויזואלים',
     description: `רכיבים ויזואלים משמעותיים שמופיעים בתבנית. למשל, תפריט צדדי, כפתורים ועוד.`,
-    customMarkdownPath: 'רכיבים-ויזואלים',
-    keywords: [],
   },
   {
     urlPath: 'תרמו-קוד',
     displayName: 'תרמו קוד לפרויקט',
     description: `מצאתם בעיה שאתם רוצים לתקן? יש לכם רעיון איך לשפר את הפרויקט או לתקן בעיות קיימות? אתם מומלצים להיכנס לעמוד הגיטהאב הרשמי שלנו ולתרום קוד!`,
-    customMarkdownPath: 'תרמו-קוד',
-    keywords: [],
   },
 ];
